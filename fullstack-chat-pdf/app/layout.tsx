fb50ceb2512a706138f300926e6955b1554855e7
unknown <ivanalbano9@gmail.com>
2025-02-11 11:47:17 +0200

    corrected the clerkprovider import

--- fullstack-chat-pdf/app/layout.tsx
+++ fullstack-chat-pdf/app/layout.tsx
@@ -1,17 +1,7 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { ClerkProvider } from "@clerk/clerk-react";
+import { ClerkProvider } from "@clerk/nextjs";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "ChatPDF",
@@ -27,10 +17,7 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en">
         <body
-          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-        >
-          {children}
-        </body>
+          className={`${geistSans.variable} ${geistMono.variable} antialiased`}> {children} </body>
       </html>
     </ClerkProvider>
   );

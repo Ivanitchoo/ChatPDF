dab349d49a092fbb64ee5b2854bd60a9114f00d1
unknown <ivanalbano9@gmail.com>
2025-02-12 09:02:06 +0200

    clerk auth built auth, first page and middleware

--- fullstack-chat-pdf/app/page.tsx
+++ fullstack-chat-pdf/app/page.tsx
@@ -1,7 +1,55 @@
-import { Button } from "@/components/ui/button"
+"use server";
 
-export default function Home() {
+import { Button } from "@/components/ui/button"
+import { UserButton } from "@clerk/nextjs"
+import { auth } from "@clerk/nextjs/server"
+import { ArrowRight, LogIn } from "lucide-react"
+import Link from "next/link"
+
+export default async function Home() {
+
+const {userId} = await auth()
+const isAuth = !!userId
+console.log("userId:", userId); // Check if userId is populated
+console.log("isAuth:", isAuth);
+
   return (
-    <h1 className="bg-red-500">Hello World!</h1>
+      <div className="w-screen min-h-screen bg-gradient-to-r from-teal-400 to-gray-800">
+        <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+          <div className="flex flex-col items-center">
+            <div className="flex items-center">
+            <h1 className="mr-8 text-5xl font-semibold">Chat with any PDF </h1>
+            <UserButton afterSignOutUrl="/" />
+            </div>
+            
+          <div className="flex mt-2">
+            {isAuth && (<Button> Go to Chats <ArrowRight/> </Button>)} 
+             
+          </div>
+
+          <p className="max-w-xl mt-1 text-lg text-slate-900 text-justify"> Join millions of students, researchers, and professionals  who are revolutionizing the way they learn and work.
+            Get precise answers, and and gain deeper insights in seconds—Smarter learning and working starts here!
+          </p>
+          
+            <div className="w-full mt-4 flex justify-center">
+              {isAuth ? (<h1>fileupload</h1>) : 
+              (
+                <Link href="/sign-in">
+                  <Button>Login to get started
+                    <LogIn className="w-4 h-4 ml-2"/>
+                  </Button>
+                  
+                </Link>
+              )}
+            </div>
+          
+          </div>
+        </div>
+      </div>
+
+
+    
+
+
   )
 }
